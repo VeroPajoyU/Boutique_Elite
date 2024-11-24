73cfe8d4a93282d77e3811eec8362b763b07a49a
CristianMusse <168245784+CristianMusse@users.noreply.github.com>
2024-11-24 18:14:02 -0500

    Update App.jsx

--- boutique_elite/src/App.jsx
+++ boutique_elite/src/App.jsx
@@ -1,9 +1,8 @@
-// App.jsx
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importa Router y Routes
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation.jsx";
 import Products from "./components/Products.jsx";
-// import Favorites from "./components/Favorites.jsx"; // Importa el componente de favoritos
+import Cart from './components/Cart.jsx'; // Importa el componente del carrito
 import fetch_data from "./api/api_backend.jsx";
 
 function App() {
@@ -13,14 +12,16 @@ function App() {
   const [colors, setColors] = useState([]);
   const [products, setProducts] = useState([]);
   const [mmPrices, setMmPrices] = useState([]);
+  const [user, setUser] = useState(null); // Simula usuario autenticado
   const [selectedCategoryId, setSelectedCategoryId] = useState(0);
   const [selectedMarksIds, setSelectedMarksIds] = useState([]);
   const [selectedSizesIds, setSelectedSizesIds] = useState([]);
   const [selectedColorsIds, setSelectedColorsIds] = useState([]);
   const [minPrice, setMinPrice] = useState(0);
   const [maxPrice, setMaxPrice] = useState(100000);
-  // const [favorites, setFavorites] = useState([]); // Estado para manejar los favoritos
   const [searchText, setSearchText] = useState('');
+
+
 
   useEffect(() => {
     fetch_data("/products", setProducts);
@@ -31,69 +32,15 @@ function App() {
     fetch_data("/products/mmprices", setMmPrices);
   }, []);
 
-  useEffect(() => {
-    if (selectedCategoryId === 0) {
-      fetch_data("/products", setProducts);
-    } else {
-      fetch_data("/products/categories/" + selectedCategoryId, setProducts);
-    }
-  }, [selectedCategoryId]);
-
-  useEffect(() => {
-    if (selectedMarksIds.length === 0) {
-      fetch_data("/products", setProducts);
-    } else {
-      const marksIdsString = selectedMarksIds.join(',');
-      fetch_data("/products/marks/" + marksIdsString, setProducts);
-    }
-  }, [selectedMarksIds]);
-
-  useEffect(() => {
-    if (selectedSizesIds.length === 0) {
-      fetch_data("/products", setProducts);
-    } else {
-      const sizesIdsString = selectedSizesIds.join(',');
-      fetch_data("/products/sizes/" + sizesIdsString, setProducts);
-    }
-  }, [selectedSizesIds]);
-
-  useEffect(() => {
-    if (selectedColorsIds.length === 0) {
-      fetch_data("/products", setProducts);
-    } else {
-      const colorsIdString = selectedColorsIds.join(',');
-      fetch_data("/products/colors/" + colorsIdString, setProducts);
-    }
-  }, [selectedColorsIds]);
-
-  useEffect(() => {
-    if (minPrice === 0 && maxPrice === 100000) {
-      fetch_data("/products", setProducts);
-    } else {
-      const priceRangeString = minPrice + "," + maxPrice;
-      fetch_data("/products/rangeprices/" + priceRangeString, setProducts);
-    }
-  }, [minPrice, maxPrice]);
-
-  useEffect(() => {
-    if (searchText==''){
-      fetch_data("/products", setProducts);
-    } else {
-      console.log(searchText)
-      fetch_data("/products/search", setProducts, { searchText }); // Enviar el texto de búsqueda en el cuerpo
-    }
-  }, [searchText]);
-
-  const handleCategorySelect = (categoryId) => { setSelectedCategoryId(categoryId) };
-  const handleMarkSelect = (markIds) => { setSelectedMarksIds(markIds) };
-  const handleSizeSelect = (sizeIds) => { setSelectedSizesIds(sizeIds) };
-  const handleColorSelect = (colorIds) => { setSelectedColorsIds(colorIds) };
+  const handleCategorySelect = (categoryId) => setSelectedCategoryId(categoryId);
+  const handleMarkSelect = (markIds) => setSelectedMarksIds(markIds);
+  const handleSizeSelect = (sizeIds) => setSelectedSizesIds(sizeIds);
+  const handleColorSelect = (colorIds) => setSelectedColorsIds(colorIds);
   const handlePriceSelect = (min, max) => {
     setMinPrice(min);
     setMaxPrice(max);
   };
-  // const handleFavoritesChange = (newFavorites) => { setFavorites(newFavorites); };
-  const handleSearchChange = (searchText) => { setSearchText(searchText); console.log('Handle: '+searchText) };
+  const handleSearchChange = (searchText) => setSearchText(searchText);
 
   return (
     <Router>
@@ -117,11 +64,12 @@ function App() {
               onSizesSelect={handleSizeSelect} 
               onColorsSelect={handleColorSelect} 
               onPriceSelect={handlePriceSelect}
-              // onFavoritesChange={handleFavoritesChange} 
               productsF={handleSearchChange} 
             />
           } />
-          {/* <Route path="/favoritos" element={<Favorites favorites={favorites} />} /> Ruta para la página de favoritos */}
+          <Route path="/carrito" element={
+            <Cart userId={user ? user.id : null} /> // Pasa el ID del usuario autenticado al carrito
+          } />
         </Routes>
       </main>
     </Router>
