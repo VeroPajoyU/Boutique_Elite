837d291a20a075b73a928c8c45670e3ebad4dd17
VeroPajoyU <yiselpajoy@unicomfacauca.edu.co>
2024-11-24 16:10:47 -0500

    Funciones de creación y verificación de Token

--- backend/js/token_authentication.js
+++ backend/js/token_authentication.js
@@ -3,3 +3,25 @@ import { KEY } from './config.js'; // This is the secret key that will be used t
 
 // This is the secret key that will be used to sign the JWT
 const SECRET_KEY = KEY;
+
+// This function generates a token with the user's data
+const generateToken = (data) => {
+    return jwt.sign(
+        user,
+        SECRET_KEY,
+        {
+            expiresIn: '1h'
+        }
+    )
+}
+
+// This function verifies the token and returns the user's data
+const verifyToken = (token) => {
+    try {
+        return jwt.verify(token, SECRET_KEY);
+    } catch (error) {
+        throw new Error ('Invalid Token');
+    }
+}
+
+export default { generateToken, verifyToken };
